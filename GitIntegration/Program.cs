80196f78c7e9b2a8cb31bd5ee34964fa0380c338
GouthamRavi <goutham.ravi@live.com>
2014-01-06 02:01:22 +0100

    Made Class

---
+++ GitIntegration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    abstract class Veichle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+    }
+
+    class Car : Veichle
+    {
+         
+    }
+}
